94e9ef20b95801c046f11fda48897fc7fd7bbae0
Vinicius-Rubia <lucas.de.oliveira.rubia1@gmail.com>
2022-06-25 14:14:21 -0300

    project finish

---
+++ README.md
@@ -0,0 +1,13 @@
+# Consulta CEP
+
+![layout](assets/img/Layout.png)
+
+----
+
+Projeto Consulta CEP usando a API viaCep para auxiliar na requisição dos dados.
+
+Desenvolvido com: 
+
+- HTML
+- jQuery e Ajax
+- Bootstrap (Framework Web)
---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+consultaCep = () => {
+    let cep = document.getElementById('cep').value;
+    let url = `https://viacep.com.br/ws/${cep}/json/`;
+    $.ajax({
+        url: url,
+        type: 'GET',
+        success: (response) => {
+            $('#logradouro').html(response.logradouro);
+            $('#bairro').html(response.bairro);
+            $('#localidade').html(response.localidade);
+            $('#uf').html(response.uf);
+            $('#titulo_cep').html(`CEP: ${response.cep}`);
+        }
+    });
+}
